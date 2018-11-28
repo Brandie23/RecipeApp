64a663ecb59bb4a8c3a95a3a9f8987be5fd7b2dd
Arry <Arry@Arry.arr>
2018-11-28 20:10:37 +1300

    recipe app baby

---
+++ starter-files-20181128T045005Z-001/starter-files/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import Recipes from "./Components/Recipes";
+const API_KEY = "8a86af0ce5213f8bad72e4f8173a1d67";
+
+class App extends Component {
+  state = {
+    recipes: []
+  };
+  getRecipe = async e => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`
+    );
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+    console.log(this.state.recipes);
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe} />
+        <Recipes recipes={this.state.recipes} />
+      </div>
+    );
+  }
+}
+
+export default App;
